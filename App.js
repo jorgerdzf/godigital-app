f2fd8367f16d8c4e4162188ac96f02fe53b6f6eb
Jorge Rdz <jorgerdzf@gmail.com>
2021-03-16 21:47:07 -0600

    initial commit

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Go Digital</Text>
       <StatusBar style="auto" />
     </View>
   );
