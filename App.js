de36ea3d073338e36c5f7bb4c336be8aafdc50f6
Jorge Rdz <jorgerdzf@gmail.com>
2021-03-16 23:19:31 -0600

    configuracion de navegacion inicial

--- App.js
+++ App.js
@@ -2,22 +2,88 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  
-  return (
-    
-    <View style={styles.container}>
-      <Text>Go Digital</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import * as firebase from 'firebase'
+import 'firebase/firestore'
+import { firebaseConfig } from './config/firebaseConfig'
+
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './redux/reducers'
+import thunk from 'redux-thunk'
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import Landing from './components/auth/Landing'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+import Main from './components/main/Main'
+import Splash from './components/main/Splash'
+import PaymentHistory from './components/payment/PaymentHistory'
+import PaymentInfo from './components/payment/PaymentInfo'
+import PaymentPost from './components/payment/PaymentPost'
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+    }
+  }
+  componentDidMount() {
+    firebase.auth().onAuthStateChange((user) => {
+      if (!user) {
+        this.setState({
+          loggedIn: false,
+          loaded: true
+        })
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true
+        })
+      }
+    });
+  }
+  render() {
+    const { loggedIn, loaded } = this.state;
+    if (!loaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <Text>Cargando...</Text>
+        </View>
+      )
+    }
+    if (!loggedIn) {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName='Landing'>
+            <Stack.Screen name='Landing' component={Landing} options={{ headerShown: false }} />
+            <Stack.Screen name='Register' component={Register} />
+            <Stack.Screen name='Login' component={Login} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      )
+    } else {
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName='Main'>
+            <Stack.Screen name='Splash' component={Splash} />
+            <Stack.Screen name='Main' component={Main} />
+            <Stack.Screen name='PaymentHistory' component={PaymentHistory} navigation={this.props.navigation}/>
+            <Stack.Screen name='PaymentInfo' component={PaymentInfo} navigation={this.props.navigation}/>
+            <Stack.Screen name='PaymentPost' component={PaymentPost} navigation={this.props.navigation}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+    }
+  }
+}
+
+export default App
---
+++ config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC7ExhKAUV7cm0W7e640h2_mwYMBejQngE",
+    authDomain: "godigital-client-app.firebaseapp.com",
+    projectId: "godigital-client-app",
+    storageBucket: "godigital-client-app.appspot.com",
+    messagingSenderId: "225265508288",
+    appId: "1:225265508288:web:0111249100a8c8cd9b3225",
+    measurementId: "G-E5B1NTYLHJ"
+  };
